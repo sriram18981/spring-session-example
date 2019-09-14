ca015fe19557c961f25b976d5ac5223e2efd5db6
Sriram Tanikella <sriram18981@gmail.com>
2019-09-14 23:50:48 +0530

    initial commit

---
+++ src/main/java/learning/sriram/spring_session_example/App.java
@@ -0,0 +1,13 @@
+package learning.sriram.spring_session_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
